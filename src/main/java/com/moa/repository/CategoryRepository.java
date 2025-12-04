35eab62344b6e5da9f155162aa34d653f4edbd93
Woojin1123 <boom175839@gmail.com>
2025-12-04 21:34:05 +0900

    fix: 카테고리 조회 방식을 ID 기반으로 수정

--- src/main/java/com/moa/repository/CategoryRepository.java
+++ src/main/java/com/moa/repository/CategoryRepository.java
@@ -3,8 +3,5 @@ package com.moa.repository;
 import com.moa.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Optional<Category> findByName(Long aLong);
 }
