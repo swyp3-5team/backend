c849ff41c64fead824c563017ffd81f47660bec3
Woojin1123 <boom175839@gmail.com>
2025-12-14 10:33:09 +0900

    refactor: 사용하지 않는 필드 제거

--- src/main/java/com/moa/dto/UserProfileResponse.java
+++ src/main/java/com/moa/dto/UserProfileResponse.java
@@ -1,17 +1,10 @@
 package com.moa.dto;
 
-import com.moa.entity.AddressType;
-import com.moa.entity.Gender;
 import com.moa.entity.UserProfile;
-
-import java.time.LocalDate;
 
 public record UserProfileResponse(
         Long profileId,
         String nickName,
-        LocalDate birth,
-        Gender gender,
-        AddressType addressType,
         boolean marketingEnable,
         boolean pushEnable,
         boolean voiceEnable,
@@ -21,9 +14,6 @@ public record UserProfileResponse(
         return new UserProfileResponse(
                 profile.getId(),
                 profile.getNickname(),
-                profile.getBirthDate(),
-                profile.getGender(),
-                profile.getAdressType(),
                 profile.isMarketingEnable(),
                 profile.isPushEnable(),
                 profile.isVoiceEnable(),
