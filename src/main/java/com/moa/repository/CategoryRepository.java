26b3cd9153ba07083681bbc2c68c5f62273fdf15
Woojin1123 <boom175839@gmail.com>
2025-12-24 18:40:41 +0900

    feat: DB 커넥션 조회를 위한 Log

--- src/main/java/com/moa/repository/CategoryRepository.java
+++ src/main/java/com/moa/repository/CategoryRepository.java
@@ -1,7 +1,14 @@
 package com.moa.repository;
 
 import com.moa.entity.Category;
+import com.moa.entity.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByNameAndType(String name, CategoryType type);
 }
