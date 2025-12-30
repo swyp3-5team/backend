6988b79ffcfafc996a01d6f8641bd99958a8b75d
이준한 <wns63560@gmail.com>
2025-12-30 17:40:30 +0900

    상태 체크 컨트롤러 추가

---
+++ src/main/java/com/moa/controller/HealthCheckController.java
@@ -0,0 +1,21 @@
+package com.moa.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        return ResponseEntity.ok(Map.of(
+                "status", "OK",
+                "message", "Server is running"
+        ));
+    }
+}
